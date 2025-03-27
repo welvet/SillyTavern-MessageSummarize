53747ccde166021ba72e09a713df05e601a1f62c
qvink <qvink@users.noreply.github.com>
2025-03-27 13:50:12 -0600

    wrapping summary injection in system prompt

--- index.js
+++ index.js
@@ -2935,8 +2935,10 @@ function refresh_memory() {
     let short_injection = get_short_memory();
 
     // if using text completion, we need to wrap it in a system prompt
-    long_injection = formatInstructModeChat("", long_injection, false, true)
-    short_injection = formatInstructModeChat("", short_injection, false, true)
+    if (main_api !== 'openai') {
+        long_injection = formatInstructModeChat("", long_injection, false, true)
+        short_injection = formatInstructModeChat("", short_injection, false, true)
+    }
 
     // inject the memories into the templates, if they exist
     ctx.setExtensionPrompt(`${MODULE_NAME}_long`,  long_injection,  get_settings('long_term_position'), get_settings('long_term_depth'), get_settings('long_term_scan'), get_settings('long_term_role'));
