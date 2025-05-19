b42da1d2e5fd0b4f6a0c6ce941b4f3c5d60ec89b
qvink <qvink@users.noreply.github.com>
2025-05-19 12:21:29 -0600

    version check no longer prevents extension from running

--- index.js
+++ index.js
@@ -298,6 +298,7 @@ function check_st_version() {
     if (getContext().symbols !== undefined) {
         return true
     } else {
+        log(`Symbols not found in context: [${getContext().symbols}]`)
         toast("Incompatible ST version - please update.", "error")
     }
 }
@@ -3712,9 +3713,7 @@ jQuery(async function () {
     const VERSION = manifest.version;
     log(`Version: ${VERSION}`)
 
-    if (!check_st_version()) {
-        return
-    }
+    check_st_version()
 
     // Load settings
     initialize_settings();
